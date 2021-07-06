3c3a2c6aea057d3c5af35b9117117ad54db7a38f
Vivek Jangid <vivekjangid608@gmail.com>
2021-07-06 23:48:53 +0530

    initial

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,9 @@
 import { Component } from '@angular/core';
+import { Platform } from '@ionic/angular';
+import { Router } from '@angular/router';
+
+import { INotificationPayload } from 'cordova-plugin-fcm-with-dependecy-updated';
+import { FCM } from 'cordova-plugin-fcm-with-dependecy-updated/ionic/ngx';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +11,49 @@ import { Component } from '@angular/core';
   styleUrls: ['app.component.scss'],
 })
 export class AppComponent {
-  constructor() {}
+  public hasPermission: boolean;
+  public token: string;
+  public pushPayload: INotificationPayload;
+
+  constructor(
+    private platform: Platform,
+    private fcm: FCM,
+    private router: Router
+  ) {
+    this.initializeApp();
+  }
+
+  private async initializeApp() {
+    await this.platform.ready();
+    console.log('FCM setup started');
+
+    if (!this.platform.is('cordova')) {
+      return;
+    }
+    console.log('In cordova platform');
+
+    console.log('Subscribing to new notifications');
+    this.fcm.onNotification().subscribe((payload) => {
+      this.pushPayload = payload;
+      console.log('onNotification received event with: ', payload);
+      try {
+        this.router.navigate([payload.landing_page, payload.price]);
+      } catch (error) {
+        alert(error);
+      }
+    });
+
+    this.hasPermission = await this.fcm.requestPushPermission();
+    console.log('requestPushPermission result: ', this.hasPermission);
+
+    this.token = await this.fcm.getToken();
+    console.log('getToken result: ', this.token);
+
+    this.pushPayload = await this.fcm.getInitialPushPayload();
+    console.log('getInitialPushPayload result: ', this.pushPayload);
+  }
+
+  public get pushPayloadString() {
+    return JSON.stringify(this.pushPayload, null, 4);
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,11 +7,14 @@ import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 
+import { FCM } from 'cordova-plugin-fcm-with-dependecy-updated/ionic/ngx';
+
+
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
   imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
-  providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
+  providers: [FCM, { provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
   bootstrap: [AppComponent],
 })
-export class AppModule {}
+export class AppModule { }
